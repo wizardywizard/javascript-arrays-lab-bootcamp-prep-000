31777157bd44363c5e2d7e6b2440131fd6a3179c
David Little <littledavid175@gmail.com>
2019-06-01 22:28:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ function destructivelyPrependKitten (bob,kitten) {
   return kittens
 }
 function destructivelyRemoveLastKitten (Garfield,kittens){
-  kittens.pop(Garfield)
+  kittens.pop(kittens)
   return kittens
 }
