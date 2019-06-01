e1b253fd98a446e4f07e2bdbfddfd70615f3b38b
David Little <littledavid175@gmail.com>
2019-06-01 23:16:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function destructivelyRemoveFirstKitten (Milo) {
 function appendKitten (kittens2) {
     return kittens.concat(kittens2)
 }
-function prependKitten (kittens) {
+function prependKitten (kittens3) {
   return [kittens,...kittens3]
 }
 function removeLastKitten (kittens) {
