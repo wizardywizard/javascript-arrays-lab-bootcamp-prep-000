90cc71947be93cd2a0153836b3adfba46fe6d8d6
David Little <littledavid175@gmail.com>
2019-06-01 22:47:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var kittens = ['Milo','Otis','Garfield']
 var kittens2 = ['Broom']
-var kittens3 = ['Arl']//define your array here
+var kittens3 = ['Arnold']//define your array here
 // Add your functions and code here
 function destructivelyAppendKitten (Ralph,kitten) {
 kittens.push(Ralph)
@@ -21,7 +21,7 @@ function destructivelyRemoveFirstKitten (Milo) {
 function appendKitten (kittens2) {
     return kittens.concat(kittens2)
 }
-function prependKitten () {
+function prependKitten (kittens3) {
   
   
 }
